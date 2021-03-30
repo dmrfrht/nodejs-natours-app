8987cc9eade7c00cd734b57986beb39eb20ca90a
dmrfrht <dmrfrht1@gmail.com>
2021-03-30 19:53:37 +0300

    end part-9

--- server.js
+++ server.js
@@ -1,16 +1,34 @@
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 
+process.on('uncaughtException', err => {
+  console.log('uncaughtException -- shutting down')
+  console.log(err.name, err.message)
+  process.exit(1)
+})
+
 dotenv.config({ path: './config.env' })
 const app = require('./app')
 
-mongoose.connect("mongodb+srv://rootUser:0246813579@cluster0.ww4aa.mongodb.net/natoursDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://rootUser:024681357@cluster0.ww4aa.mongodb.net/natoursDB?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
   useUnifiedTopology: true
-}).then(() => console.log("db connection successfuly"))
+})
+  .then(() => console.log("db connection successfuly"))
 
-app.listen(process.env.PORT || 3000, () => {
+
+const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`App running on port: ${process.env.PORT}`)
 })
+
+process.on('unhandledRejection', err => {
+  console.log('unhandledRejection -- shutting down')
+  console.log(err.name, err.message)
+  server.close(() => {
+    process.exit(1)
+  })
+})
+
+// console.log(x)
